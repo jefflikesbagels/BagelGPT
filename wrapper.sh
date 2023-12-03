1fca78b3162d1f652f46853977188f1e0efd7405
jefflikesbagels <jeff@jefflikesbagels.net>
2023-12-03 00:10:12 -0500

    updated wrapper

--- wrapper.sh
+++ wrapper.sh
@@ -7,8 +7,8 @@ python3 main.py
 return_code=$?
 
 # Check the return code
-if [ $return_code -eq 123 ]; then
-    echo "main.py returned the expected code 123. Performing additional actions..."
+if [ $return_code -eq 1001 ]; then
+    echo "main.py returned the expected code 1001. Performing additional actions..."
     # Perform additional actions here
 else
     echo "main.py returned a different code. Exiting."
