32924977bb603212957382738eb4fda43fae96b7
jefflikesbagels <jeff@jefflikesbagels.net>
2023-12-02 23:41:08 -0500

    Clean up comments

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
 from bagelgpt import bagelgpt
 import discord
-from discord.ext import tasks
 from discord.ext import commands
 import os
 import asyncio
 from dotenv import load_dotenv
 import subprocess
 
-# loading environment variables from .env file
+# Load environment variables from .env file
 load_dotenv()
 
-# Getting API keys from environment variables
+# Get API keys and channel IDs from environment variables
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 OPENAI_MODEL = os.environ.get('OPENAI_MODEL')
 DISCORD_API_KEY = os.environ.get('DISCORD_API_KEY')
 DISCORD_GENERAL_CHANNEL = os.environ.get('DISCORD_GENERAL_CHANNEL')
 DISCORD_RAP_BOT_CHANNEL = os.environ.get('DISCORD_RAP_BOT_CHANNEL')
 
-# Initializing the gpt class in the bagelgpt library
+# Initialize the gpt class in the bagelgpt library
 gpt = bagelgpt(api_key=OPENAI_API_KEY, model=OPENAI_MODEL)
 
-# Setting up Discord Bot
+# Set up the Discord Bot
 intents = discord.Intents.default()  # getting default intents for Discord API
 intents.message_content = True  # enabling message content intent for Discord API
 bot = commands.Bot(command_prefix='/', intents=intents)
 history_limit = 10
 conversation_history = {}
 
-# Defining event handler for when the Discord Bot is ready
+# Define event handler for when the Discord Bot is ready
 @bot.event
 async def on_ready():
     # Printing a message to the console to confirm that the Bot is ready
     print(f'{bot.user} has connected to Discord!')
       
-    channel = bot.get_channel(int(DISCORD_GENERAL_CHANNEL))  # Get the Discord channel object for Chit-Chat
+    # Get the Discord "General" channel object
+    channel = bot.get_channel(int(DISCORD_GENERAL_CHANNEL))
     await channel.send("I'm back baby!")
 
-# Defining a task to run GPT-3.5 chatbot in channel 0Z every 10 seconds
+# Define an event handler for responding to messages in the "Rap-Bot" channel
 @bot.event
 async def on_message(message):
     global history_limit
@@ -44,6 +44,7 @@ async def on_message(message):
     print(f"Received message: {message.content}")
     await bot.process_commands(message)
 
+    # Get the Discord "Rap-Bot" channel object
     channel = bot.get_channel(int(DISCORD_RAP_BOT_CHANNEL))
 
     # Ignore messages from the bot or other channels
@@ -76,12 +77,14 @@ async def on_message(message):
     response = gpt.get_chat_completion(message.content, context, channel.topic)
     print(f"Got response from GPT:", response)
 
+    # Delete the original attempting to create response message and insert the final response
     await response_message.delete()
     await channel.send(response)
 
 async def history_command(message):
     global history_limit
 
+    # Update the message history context with /history=<limit>
     try:
         new_limit = int(message.content.split('=')[1])
         history_limit = new_limit
